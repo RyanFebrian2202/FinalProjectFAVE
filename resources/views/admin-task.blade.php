79215778f181736f7809fab97668b4464b1c6f3b
RyanFebrian2202 <ryan22febrian@gmail.com>
2022-08-31 19:26:42 +0700

    [finish] admin : finish admin feature

--- resources/views/admin-task.blade.php
+++ resources/views/admin-task.blade.php
@@ -110,6 +110,7 @@
                         </div>
                     </div>
                     @endforeach
+                    {{$tasks->links()}}
                 </section>
         </div>
 
